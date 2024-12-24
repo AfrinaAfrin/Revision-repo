eb0f43106683e813b487ccca52b9c1d4449511d5
AfrinaAfrin <afrinaafrin1033@gmail.com>
2024-12-24 14:03:46 +0530

    added new file

---
+++ Q.08.js
@@ -0,0 +1,37 @@
+ 
+function Animal() {
+    this.type = "Animal";
+}
+
+ 
+Animal.prototype.sound = function () {
+    console.log("Animal sound");
+};
+
+ 
+function Dog() {
+    Animal.call(this);  
+    this.type = "Dog";  
+}
+
+ 
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog; 
+ 
+Dog.prototype.sound = function () {
+    console.log("Bark");
+};
+
+ 
+const myDog = new Dog();
+
+ 
+myDog.sound(); 
+
+ 
+const anotherAnimal = new Animal();
+anotherAnimal.sound();  
+
+console.log(myDog instanceof Dog);  
+console.log(myDog instanceof Animal);  
+console.log(anotherAnimal instanceof Dog);
